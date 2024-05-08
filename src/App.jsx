9ab95639cf1c2f9a7349ef6c2332a00c9b0cf784
sayakmallick4875 <sayak@solivatech.com>
2024-05-08 14:38:58 +0530

    feat: Add header component to main app page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import './App.css'
+import Header from './components/Header'
 
 export default function App() {
   return (
     <main>
       <div>
-        <h1>Fake Store</h1> 
+        <Header />
       </div>  
     </main>
   )
